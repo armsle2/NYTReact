06928d38bc54a171b493164aef12a81b7b450a49
Leaveil <leaveil.armstrong@gmail.com>
2018-02-09 13:55:05 -0500

    fixed module name

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const articlesController = require("../../controllers/articlesController");
+const articlesController = require("../../controllers/articlescontroller");
 
 // get saved articles and save a new article
 router
