d80921115c52bd12f79ed20e2ccd974c639ed50c
Leaveil <leaveil.armstrong@gmail.com>
2018-02-10 13:54:05 -0500

    test 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Articles from "./components"
+import Articles from "./components/Home"
 
 class App extends Component {
   render() {
